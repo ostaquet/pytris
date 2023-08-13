8800ee502eaf1b6caaaec2433fd0c24eebad6e92
Olivier Staquet <olivier.staquet@gabismartcare.com>
2023-08-13 10:50:45 +0200

    Add management of command and move (only on tetrimino O)

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from enum import Enum
+from typing import List
 import pygame
+
+
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 class Cell(Enum):
@@ -11,6 +18,52 @@ class Cell(Enum):
     LIGHT_BLUE = 4
     DARK_BLUE = 5
     PURPLE = 6
+
+
+class Command(Enum):
+    NONE = 0
+    LEFT = 1
+    RIGHT = 2
+    DOWN = 3
+    TURN = 4
+
+
+class TetrinimoO:
+    color: Cell = Cell.YELLOW
+
+    def __init__(self):
+        self.pos: Position = Position(5, 0)
+
+    def get_positions(self) -> List[Position]:
+        return [Position(self.pos.x, self.pos.y),
+                Position(self.pos.x+1, self.pos.y),
+                Position(self.pos.x, self.pos.y+1),
+                Position(self.pos.x+1, self.pos.y+1)]
+
+    def try_command(self, command: Command) -> list[Position]:
+        positions: list[Position] = self.get_positions()
+
+        if command == Command.DOWN:
+            for pos in positions:
+                pos.y = pos.y + 1
+
+        if command == Command.LEFT:
+            for pos in positions:
+                pos.x = pos.x - 1
+
+        if command == Command.RIGHT:
+            for pos in positions:
+                pos.x = pos.x + 1
+
+        return positions
+
+    def apply_command(self, command: Command) -> None:
+        if command == Command.DOWN:
+            self.pos.y = self.pos.y + 1
+        if command == Command.LEFT:
+            self.pos.x = self.pos.x - 1
+        if command == Command.RIGHT:
+            self.pos.x = self.pos.x + 1
 
 
 # Set up colors
@@ -29,6 +82,23 @@ white = (255, 255, 255)
 square_size = 25
 
 
+def is_position_valid(future_positions, board):
+    for pos in future_positions:
+        if pos.x < 0 or pos.x >= 12:
+            return False
+        if pos.y < 0 or pos.y >= 22:
+            return False
+        if board[pos.y][pos.x] != Cell.EMPTY:
+            return False
+
+    return True
+
+
+def freeze(current_tetromino, board):
+    for pos in current_tetromino.get_positions():
+        board[pos.y][pos.x] = current_tetromino.color
+
+
 def main():
     board = [[Cell.EMPTY for _ in range(12)] for _ in range(22)]
 
@@ -36,13 +106,18 @@ def main():
     pygame.init()
 
     # Set up the game window
-    window_width = 800
-    window_height = 600
+    window_width: int = 800
+    window_height: int = 600
     window = pygame.display.set_mode((window_width, window_height))
     pygame.display.set_caption("Pytris")
 
     # Game variables
-    running = True
+    running: bool = True
+
+    current_tetromino = TetrinimoO()
+    current_command: Command = Command.NONE
+    count_cannot_go_down: int = 0
+    last_tick: int = pygame.time.get_ticks()
 
     # Main game loop
     while running:
@@ -50,22 +125,40 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_command = Command.LEFT
+                if event.key == pygame.K_RIGHT:
+                    current_command = Command.RIGHT
+                if event.key == pygame.K_DOWN:
+                    current_command = Command.DOWN
+                if event.key == pygame.K_SPACE:
+                    current_command = Command.TURN
 
         # ****************** Update game logic ******************
-        board[10][10] = Cell.PURPLE
-        board[11][10] = Cell.PURPLE
-        board[11][11] = Cell.PURPLE
-        board[12][10] = Cell.PURPLE
+        # if is_collision(current_tetromino, board):
+        #     # Game over
+        #     running = False
+        #
+        future_positions: list[Position] = current_tetromino.try_command(current_command)
+        if is_position_valid(future_positions, board):
+            current_tetromino.apply_command(current_command)
+        current_command = Command.NONE
 
-        board[1][1] = Cell.LIGHT_BLUE
-        board[2][1] = Cell.LIGHT_BLUE
-        board[3][1] = Cell.LIGHT_BLUE
-        board[4][1] = Cell.LIGHT_BLUE
+        if pygame.time.get_ticks() - last_tick > 150:
+            if count_cannot_go_down >= 2:
+                freeze(current_tetromino, board)
+                current_tetromino = TetrinimoO()
 
-        board[5][4] = Cell.YELLOW
-        board[5][5] = Cell.YELLOW
-        board[4][4] = Cell.YELLOW
-        board[4][5] = Cell.YELLOW
+            future_positions: list[Position] = current_tetromino.try_command(Command.DOWN)
+
+            if is_position_valid(future_positions, board):
+                current_tetromino.apply_command(Command.DOWN)
+                count_cannot_go_down = 0
+            else:
+                count_cannot_go_down = count_cannot_go_down + 1
+
+            last_tick = pygame.time.get_ticks()
 
         # ****************** Draw the game ******************
         # Clear the screen
@@ -85,6 +178,15 @@ def main():
                      square_size,
                      square_size))
 
+        for pos in current_tetromino.get_positions():
+            pygame.draw.rect(
+                window,
+                get_color_for(current_tetromino.color),
+                (origin_x + (pos.x * square_size),
+                 (origin_y + (pos.y * square_size)),
+                 square_size,
+                 square_size))
+
         # Update the display
         pygame.display.update()
 
@@ -92,7 +194,7 @@ def main():
     pygame.quit()
 
 
-def get_color_for(cell):
+def get_color_for(cell: Cell) -> tuple[int, int, int]:
     if cell == Cell.EMPTY:
         return dark_grey
     elif cell == Cell.RED:
